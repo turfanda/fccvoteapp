fa9ae0e2b98f7fa0196d392f8023e6c74aa0997f
Glitch (fcc-vote-app) <none>
2017-12-29 06:34:29 +0000

    🚙🍚 Checkpoint

--- model/user.js
+++ model/user.js
@@ -12,7 +12,7 @@ var userDataSchema = new Schema({
 
 var userData = mongoose.model("userData",userDataSchema)
 
-module.exports.createUser = function(newUser, callback){
+var createUser = function(newUser, callback){
 	bcrypt.genSalt(10, function(err, salt) {
 	    bcrypt.hash(newUser.password, salt, function(err, hash) {
 	        newUser.password = hash;
@@ -23,3 +23,4 @@ module.exports.createUser = function(newUser, callback){
 
 
 module.exports = userData;
+module.exports = createUser;
