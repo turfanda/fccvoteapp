d3404c4b9739da4329991568df427e0c3601baab
Glitch (fcc-vote-app) <none>
2017-12-30 09:47:49 +0000

    🎣🏘 Checkpoint public/js/main.js:596315 views/dashboard.hbs:596315

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
-var x=0;
+var char=64;
 
 $(function(){
 $("#addOption").on("click",function(event ){
 event.preventDefault();
-$("<input type='text'>").attr("id","")
-
+var y =$("<li>")
+var x = $("<input type='text'>").attr("id",String.fromCharCode(char+1)).attr("name",String.fromCharCode(char+1))
+var z =y.append(x);
+$("#options").append(z);  
 });
-
-
 })
