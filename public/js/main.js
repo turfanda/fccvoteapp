0ca2974668f214a12cec4c3e5d638e0707396aca
Glitch (fcc-vote-app) <none>
2017-12-30 09:58:50 +0000

    ⛱🙌 Checkpoint views/dashboard.hbs:596315 public/css/style.css:596315 public/js/main.js:596315

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,26 @@
 var char=64;
 
 $(function(){
-$("#addOption").on("click",function(event ){
-event.preventDefault();
-var y =$("<li>")
-var x = $("<input type='text'>").attr("id",String.fromCharCode(char+1)).attr("name",String.fromCharCode(char+1))
-var z =y.append(x);
-$("#options").append(z);  
-});
+  $("#addOption").on("click",function(event ){
+    event.preventDefault();
+    var y =$("<li>")
+    var x = $("<input type='text'>").attr("id",String.fromCharCode(char+1)).attr("name",String.fromCharCode(char+1))
+    var z =y.append(x);
+    $("#options").append(z);
+
+    if($("li").length===0){
+      $("#deleteOption").css("display","none")
+    }
+    else{
+      $("#deleteOption").css("display","block");
+    }
+  });
+  
+  $("#deleteOption").on("click",function(event ){
+    event.preventDefault();
+    var s = "#"+String.fromCharCode(char);
+    $(s).remove();
+    char=char-1;
+
+  });
 })
