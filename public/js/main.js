de53b73abd9f751b40b7835ca2563f5b14039cec
Glitch (fcc-vote-app) <none>
2018-01-02 06:07:43 +0000

    🏤💀 Checkpoint public/js/main.js:596315

--- public/js/main.js
+++ public/js/main.js
@@ -12,6 +12,10 @@ function getAllPollonLoad(){
                 var x = $("<div>").addClass("panel panel-primary").attr("id",item.pollName)
                 .append($("<div>").addClass("panel-heading clickable").text(item.pollName))
                 .append($("<div>").addClass("panel-body").text(item.pollQuestion));
+                for(var i=1;i<item.optionCount+1;i++){
+                var z = String.fromCharCode(char+i)
+                x.append($("<input type='radio' name='options'>").text(item.z));
+                }
                 groupPanel.append(x);
               });
               $(".jumbotron").append(groupPanel);
@@ -32,7 +36,7 @@ $(function(){
     var y =$("<li>")
     var x = $("<input type='text'>").attr("id",String.fromCharCode(char+1)).attr("name",String.fromCharCode(char+1))
     char=char+1;
-    $("#optionCount").val($("#optionCount").val()+1);
+    $("#optionCount").val(parseInt($("#optionCount").val())+1);
     var z =y.append(x);
     $("#options").append(z);
 
@@ -43,7 +47,7 @@ $(function(){
     event.preventDefault();
     $("li:last").remove();
     char=char-1;
-    $("#optionCount").val($("#optionCount").val()-1);
+    $("#optionCount").val(parseInt($("#optionCount").val())-1);
     
     if($("#options li").length===0){
       $("#deleteOption").css("display","none")
