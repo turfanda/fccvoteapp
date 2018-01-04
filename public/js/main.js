a0fe0a6a4528ad24e0f57d4b39ed04ba2b869335
Glitch (fcc-vote-app) <none>
2018-01-04 05:32:10 +0000

    🚀😜 Checkpoint public/js/main.js:596315

--- public/js/main.js
+++ public/js/main.js
@@ -162,26 +162,21 @@ $(function(){
 
               var pollChartjs = new Chart(ctx, {
                   type: 'pie',
-                  data: {
-                      labels: [result.A, result.B, result.C, result.D],
-                      datasets: [{
-                          label: '# of Votes',
-                          data: [result.Act, result.Dct, result.Cct, result.Dct, ],
-                          backgroundColor: [
-                              'rgba(255, 99, 132, 0.2)',
-                              'rgba(54, 162, 235, 0.2)',
-                              'rgba(255, 206, 86, 0.2)',
-                              'rgba(75, 192, 192, 0.2)',
-                          ],
-                          borderColor: [
-                              'rgba(255,99,132,1)',
-                              'rgba(54, 162, 235, 1)',
-                              'rgba(255, 206, 86, 1)',
-                              'rgba(75, 192, 192, 1)',
-                          ],
-                          borderWidth: 1
-                      }]
-                  },
+                data : {
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+},  
+                
+                
+                
                   options: {
                       scales: {
                           yAxes: [{
@@ -201,3 +196,25 @@ $(function(){
   
   
 })
+
+
+/*data: {
+                      labels: [result.A, result.B, result.C, result.D],
+                      datasets: [{
+                          label: '# of Votes',
+                          data: [result.Act, result.Dct, result.Cct, result.Dct, ],
+                          backgroundColor: [
+                              'rgba(255, 99, 132, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(255, 206, 86, 0.2)',
+                              'rgba(75, 192, 192, 0.2)',
+                          ],
+                          borderColor: [
+                              'rgba(255,99,132,1)',
+                              'rgba(54, 162, 235, 1)',
+                              'rgba(255, 206, 86, 1)',
+                              'rgba(75, 192, 192, 1)',
+                          ],
+                          borderWidth: 1
+                      }]
+                  },
