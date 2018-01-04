09f14b3a10571b0361eaff5f943b74eea09d252e
Glitch (fcc-vote-app) <none>
2018-01-04 05:28:11 +0000

    🎀🍩 Checkpoint

--- public/js/main.js
+++ public/js/main.js
@@ -147,58 +147,55 @@ $(function(){
         });
   });
   
-  $(document).on("click",".showResult",function(){
-    var data ={
-    "pollName":$(this).parent().attr("id")
-    }
-    $.ajax({
-            type: 'post',
-            url: "/getPollResult",
-            dataType: 'json',
-            data:data,
-            success:function(result){
+  $(document).on("click", ".showResult", function() {
+      var data = {
+          "pollName": $(this).parent().attr("id")
+      }
+      $.ajax({
+          type: 'post',
+          url: "/getPollResult",
+          dataType: 'json',
+          data: data,
+          success: function(result) {
               console.log(result);
-                var ctx = $("#pollChart");
-  
-                var pollChartjs= new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [result.A, result.B, result.C, result.D],
-        datasets: [{
-            label: '# of Votes',
-            data: [result.Act,result.Dct,result.Cct,result.Dct,],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+              var ctx = $("#pollChart");
 
-                $("#pollResult").modal("show");
-            }
-        });
-    
+              var pollChartjs = new Chart(ctx, {
+                  type: 'bar',
+                  data: {
+                      labels: [result.A, result.B, result.C, result.D],
+                      datasets: [{
+                          label: '# of Votes',
+                          data: [result.Act, result.Dct, result.Cct, result.Dct, ],
+                          backgroundColor: [
+                              'rgba(255, 99, 132, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(255, 206, 86, 0.2)',
+                              'rgba(75, 192, 192, 0.2)',
+                          ],
+                          borderColor: [
+                              'rgba(255,99,132,1)',
+                              'rgba(54, 162, 235, 1)',
+                              'rgba(255, 206, 86, 1)',
+                              'rgba(75, 192, 192, 1)',
+                          ],
+                          borderWidth: 1
+                      }]
+                  },
+                  options: {
+                      scales: {
+                          yAxes: [{
+                              ticks: {
+                                  beginAtZero: true
+                              }
+                          }]
+                      }
+                  }
+              });
 
-  
+              $("#pollResult").modal("show");
+          }
+      });
   });
   
   
