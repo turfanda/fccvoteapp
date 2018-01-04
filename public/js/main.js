8fffa1f1f20a04e96171ed41b8fe7487a7112e43
Glitch (fcc-vote-app) <none>
2018-01-04 11:36:32 +0000

    🐆🍋 Checkpoint

--- public/js/main.js
+++ public/js/main.js
@@ -17,10 +17,10 @@ function getAllUserPollonLoad(){
                 var ww=$("<button>").addClass("btn btn-secondary showResult").text("Show Result");
                 var ss=$("<button>").addClass("btn btn-info shareResult").text("Share Result");
                 var zz=$("<button>").addClass("btn btn-danger deletePoll").text("DeletePoll");
-                var tweet=$("<a>").addClass("btn ")
+                var tweet=$("<a>").addClass("btn btn-twitter").text("Share")
                 var x = $("<div>").addClass("panel panel-primary ")
                 .append($("<div>").addClass("panel-heading").text(item.pollName).append("<span class='pull-right clickable panel-collapsed'><i class='fa fa-arrow-up'></i></span>"))
-                .append($("<div>").addClass("panel-body").attr("id",item.pollName).text(item.pollQuestion).append(y).append(qq).append(ww).append(ss).append(zz));
+                .append($("<div>").addClass("panel-body").attr("id",item.pollName).text(item.pollQuestion).append(y).append(qq).append(ww).append(ss).append(zz).append(tweet));
                 groupPanel.append(x);
               });
               $("#dashjumbotron").append(groupPanel);
