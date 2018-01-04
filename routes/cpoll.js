01492ad3e378e2819d4908513b37ad94c052ab36
Glitch (fcc-vote-app) <none>
2018-01-04 19:26:44 +0000

    💳🎰 Checkpoint routes/cpoll.js:596315

--- routes/cpoll.js
+++ routes/cpoll.js
@@ -30,11 +30,13 @@ router.post("/",Common.ensureAuthenticated,function(req,res,next){
             });
             Poll.createPoll(newPoll,function(err,poll){
               if(err) throw err;
+              else{
+            req.flash('basarili_mesaj', 'Poll generated you can vote now');
+            res.redirect('/dashboard');
+              }
 
             });
-            req.flash('basarili_mesaj', 'Poll generated you can vote now');
-            console.log(1);
-            res.redirect('/dashboard');
+
           }
         }
     });
